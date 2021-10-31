80d1cb16f0baafeae1d358e8d10613adc3e2aab0
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-10-31 20:54:00 +0100

    Create list and item component for resource list

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+export default {
+  components: {
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'offical-guide',
+          title: 'Official Guide',
+          description: 'The offical Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to goole...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
